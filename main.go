7b2cf4839e7b87987f68c642e5640d70eeb9d687
Yun Zhang <yunzhang@databricks.com>
2018-05-01 17:58:32 -0700

    v2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example version 2"
 
 	from := ""
 	if r.URL != nil {
